887f615d1fb50b195b9b5312bb5bb229b7dc0435
CristianMeSu <cristian.mendoza.026@gmail.com>
2025-04-16 16:59:30 -0500

    Refactor code structure for improved readability and maintainability

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,13 +5,22 @@ module.exports = {
     "./public/**/*.html", // Si tienes archivos HTML en public/
   ],
   theme: {
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1600px',
+    },
     extend: {colors: {
       'primary-color': 'var(--primary-color)',
     },},
     fontFamily: {
       lindsey: ['"lindsey-signature"', 'sans-serif'],
       alverata: ['"alverata"', 'sans-serif'],
-      bebas: ['"bebas-neue"', 'sans-serif'],
+      bebas: ['"bebas"', 'sans-serif'],
+      bebasBold: ['"bebas-bold"', 'sans-serif'],
+      lato: ['"lato"', 'sans-serif'],
+      latoBold: ['"lato-bold"', 'sans-serif'],
     },
   },
   plugins: [],

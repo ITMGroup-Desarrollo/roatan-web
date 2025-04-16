2a5ed3a407672631d1f7d3b27f7e969b5051f010
CristianMeSu <cristian.mendoza.026@gmail.com>
2025-04-16 17:27:09 -0500

    Add AOS library for scroll animations in main.js

---
+++ src/scripts/main.js
@@ -0,0 +1,15 @@
+// src/scripts/main.js
+import AOS from "aos";
+import "aos/dist/aos.css";
+
+document.addEventListener("DOMContentLoaded", () => {
+  AOS.init();
+});
+//   data-aos="fade-up"
+//   data-aos-offset="200"
+//   data-aos-delay="300"
+//   data-aos-duration="1000"
+//   data-aos-easing="ease-in-out"
+//   data-aos-mirror="true"
+//   data-aos-once="false"
+//   data-aos-anchor-placement="top-center"

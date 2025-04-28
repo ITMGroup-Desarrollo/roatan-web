21033691f74662c4cfd0c9219c4dd8a8442a9f18
CristianMeSu <cristian.mendoza.026@gmail.com>
2025-04-28 12:43:02 -0500

    fix: remove unused i18n integration from astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 import { defineConfig } from 'astro/config';
-import i18n from '@astro/i18n';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   integrations: [
-    i18n(),
     tailwind(),
   ],
 });

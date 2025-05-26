dcf8e33ca1be398866e436d54412ae2f82c524ae
CristianMeSu <cristian.mendoza.026@gmail.com>
2025-05-26 09:46:47 -0500

    feat: add Travel and Discover components with wave animation effects

--- src/locales.js
+++ src/locales.js
@@ -1,40 +1,43 @@
-
-
 export const translations = {
   //region Español
   es: {
     menu: {
-      "home": "Inicio",
+      home: "Inicio",
       "port-experience": "Port Experience",
       "discover-beyond": "Beyond the cruise",
-      "blog": "Blog",
-      "faqs": "FAQ\'s",
-      "contact": "Contacto",
+      blog: "Blog",
+      faqs: "FAQ's",
+      contact: "Contacto",
     },
     header: {
-      "title": "Roatan es",
-      "subtitle": "Conexión|Humano|Transformación|Emoción|Impacto|Narrativa|Energía|Inspiración|Sensación|Creatividad|Inmersión|Innovación|Compromiso|Descubrimiento|Vida|Movimiento|Evolución|Todo",
-      "button": "leer más",
+      title: "Roatan es",
+      subtitle:
+        "Conexión|Humano|Transformación|Emoción|Impacto|Narrativa|Energía|Inspiración|Sensación|Creatividad|Inmersión|Innovación|Compromiso|Descubrimiento|Vida|Movimiento|Evolución|Todo",
+      button: "leer más",
     },
-    notFound:{
-      "notFound": "Ups! parece que te has perdido",
-      "back": "Regreso al inicio",
+    notFound: {
+      notFound: "Ups! parece que te has perdido",
+      back: "Regreso al inicio",
     },
-    footer:{
-      "textboxPlaceholder": "¡Unete al newsletter!",
-      "textboxButton": "suscríbete",
-      "policy": "Política de privacidad",
+    footer: {
+      textboxPlaceholder: "¡Unete al newsletter!",
+      textboxButton: "suscríbete",
+      policy: "Política de privacidad",
     },
-    homeWelcome:{
-      "title": "WELCOME TO",
-      "subtitle": "PORT ROATAN",
-      "text": "Mollit sit enim in eiusmod laborum sunt commodo cupidatat pariatur laborum consectetur magna anim anim. Veniam aliquip ut sint ex esse amet dolor eiusmod incididunt reprehenderit reprehenderit sunt ipsum.",
+    homeWelcome: {
+      title: "WELCOME TO",
+      subtitle: "PORT ROATAN",
+      text: "Mollit sit enim in eiusmod laborum sunt commodo cupidatat pariatur laborum consectetur magna anim anim. Veniam aliquip ut sint ex esse amet dolor eiusmod incididunt reprehenderit reprehenderit sunt ipsum.",
     },
-    homePort:{
-      "title": "Port Experience",
-      "subtitle": "lorem ipsum dolor sit amet",
-      "button": "leer más",
-      "text": "Mollit sit enim in eiusmod laborum sunt commodo cupidatat pariatur laborum consectetur magna anim anim. Veniam aliquip ut sint ex esse amet dolor eiusmod incididunt reprehenderit reprehenderit sunt ipsum.",
+    homePort: {
+      title: "Port Experience",
+      subtitle: "lorem ipsum dolor sit amet",
+      button: "leer más",
+      text: "Mollit sit enim in eiusmod laborum sunt commodo cupidatat pariatur laborum consectetur magna anim anim. Veniam aliquip ut sint ex esse amet dolor eiusmod incididunt reprehenderit reprehenderit sunt ipsum.",
+    },
+    homeTravel: {
+      title: "Viajar para crear",
+      subtitle: "memorias alrededor del mundo",
     },
   },
   //endregion
@@ -42,37 +45,42 @@ export const translations = {
   //region Inglés
   en: {
     menu: {
-      "home": "Home",
+      home: "Home",
       "port-experience": "Port Experience",
       "discover-beyond": "Beyond the cruise",
-      "blog": "Blog",
-      "faqs": "FAQ\'s",
-      "contact": "Contact",
+      blog: "Blog",
+      faqs: "FAQ's",
+      contact: "Contact",
     },
     header: {
-      "title": "Roatan is",
-      "subtitle": "Connection|Human|Transformation|Emotion|Impact|Storytelling|Energy|Inspiration|Sensation|Creativity|Immersion|Innovation|Engagement|Discovery|Life|Movement|Evolution|Everywhere|Everything",
-      "button": "read more",
+      title: "Roatan is",
+      subtitle:
+        "Connection|Human|Transformation|Emotion|Impact|Storytelling|Energy|Inspiration|Sensation|Creativity|Immersion|Innovation|Engagement|Discovery|Life|Movement|Evolution|Everywhere|Everything",
+      button: "read more",
     },
-    notFound:{
-      "notFound": "Ups! it looks like you\'re lost",
-      "back": "Back to home",
+    notFound: {
+      notFound: "Ups! it looks like you're lost",
+      back: "Back to home",
     },
-    footer:{
-      "textboxPlaceholder": "Join our newsletter!",
-      "textboxButton": "subscribe",
-      "policy": "Privacy Policy",
+    footer: {
+      textboxPlaceholder: "Join our newsletter!",
+      textboxButton: "subscribe",
+      policy: "Privacy Policy",
     },
-    homeWelcome:{
-      "title": "WELCOME TO",
-      "subtitle": "PORT ROATAN",
-      "text": "Mollit sit enim in eiusmod laborum sunt commodo cupidatat pariatur laborum consectetur magna anim anim. Veniam aliquip ut sint ex esse amet dolor eiusmod incididunt reprehenderit reprehenderit sunt ipsum. ",
+    homeWelcome: {
+      title: "WELCOME TO",
+      subtitle: "PORT ROATAN",
+      text: "Mollit sit enim in eiusmod laborum sunt commodo cupidatat pariatur laborum consectetur magna anim anim. Veniam aliquip ut sint ex esse amet dolor eiusmod incididunt reprehenderit reprehenderit sunt ipsum. ",
     },
-    homePort:{
-      "title": "Port Experience",
-      "subtitle": "lorem ipsum dolor sit amet",
-      "button": "Read more",
-      "text": "Mollit sit enim in eiusmod laborum sunt commodo cupidatat pariatur laborum consectetur magna anim anim. Veniam aliquip ut sint ex esse amet dolor eiusmod incididunt reprehenderit reprehenderit sunt ipsum.",
+    homePort: {
+      title: "Port Experience",
+      subtitle: "lorem ipsum dolor sit amet",
+      button: "Read more",
+      text: "Mollit sit enim in eiusmod laborum sunt commodo cupidatat pariatur laborum consectetur magna anim anim. Veniam aliquip ut sint ex esse amet dolor eiusmod incididunt reprehenderit reprehenderit sunt ipsum.",
+    },
+    homeTravel: {
+      title: "Travel to make",
+      subtitle: "memories all around the world",
     },
   },
   //endregion

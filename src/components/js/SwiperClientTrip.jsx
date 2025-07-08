63cbc2c7aa05534e56ca61452acbf956a3c995e1
CristianMeSu <cristian.mendoza.026@gmail.com>
2025-07-08 10:37:09 -0500

    fix: update slides data with TripAdvisor reviews and improve slide component structure

--- src/components/js/SwiperClientTrip.jsx
+++ src/components/js/SwiperClientTrip.jsx
@@ -9,43 +9,31 @@ import "../../assets/css/home.css";
 
 const slidesData = [
   {
-    img: "1.png",
-    text: "Sodales ut etiam sit amet nisi. Semper feugiat nibh sed pulvinarproin amet nulla morbi.",
-    name: "Isabelle Layson",
+    img: "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/1a/f6/e4/2d/default-avatar-2020-48.jpg?w=100&h=100&s=1",
+    text: "The Port of Roatan was a great port to get off the boat and hang out...",
+    name: "Susan I",
+    url: "https://www.tripadvisor.com.mx/ShowUserReviews-g292019-d25290415-r1015268803-Port_Of_Roatan-Roatan_Bay_Islands.html",
   },
   {
-    img: "2.png",
-    text: "Sodales ut etiam sit amet nisi. Semper feugiat nibh sed pulvinarproin amet nulla morbi.",
-    name: "Isabelle Layson",
+    img: "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/01/05/3a/00/privatemf.jpg?w=100&h=100&s=1",
+    text: "This was a beautiful port and had a lot of great shopping, local culture and recreation...",
+    name: "privatemf",
+    url: "https://www.tripadvisor.com.mx/ShowUserReviews-g292019-d25290415-r953307467-Port_Of_Roatan-Roatan_Bay_Islands.html",
   },
   {
-    img: "3.png",
-    text: "Sodales ut etiam sit amet nisi. Semper feugiat nibh sed pulvinarproin amet nulla morbi.",
-    name: "Isabelle Layson",
-  },
-  {
-    img: "1.png",
-    text: "Sodales ut etiam sit amet nisi. Semper feugiat nibh sed pulvinarproin amet nulla morbi.",
-    name: "Isabelle Layson",
-  },
-  {
-    img: "2.png",
-    text: "Sodales ut etiam sit amet nisi. Semper feugiat nibh sed pulvinarproin amet nulla morbi.",
-    name: "Isabelle Layson",
-  },
-  {
-    img: "3.png",
-    text: "Sodales ut etiam sit amet nisi. Semper feugiat nibh sed pulvinarproin amet nulla morbi.",
-    name: "Isabelle Layson",
-  },
+    img: "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/18/52/63/ec/caption.jpg?w=100&h=100&s=1",
+    text: 'This is a very "disneyfied" port where you have all the tourist shops and restaurants...',
+    name: "BrandonK850",
+    url: "https://www.tripadvisor.com.mx/ShowUserReviews-g292019-d25290415-r996189449-Port_Of_Roatan-Roatan_Bay_Islands.html",
+  }
 ];
 
-function Slide({ img, name, text }) {
+function Slide({ img, name, text, url }) {
   return (
     <div className="swiper-slide md:mt-5 mb-16 h-full flex justify-center items-center  content-center">
-      <div className="slide-content relative justify-self-center bg-white rounded-3xl p-6 shadow-lg text-center w-72 transition-transform duration-300">
+      <a href={`${url}`} target="blank" className="slide-content cursor-pointer relative justify-self-center bg-white rounded-3xl p-6 shadow-lg text-center w-72 transition-transform duration-300">
         <img
-          src={`/img/home/recursos/swiperTrip/${img}`}
+          src={`${img}`}
           alt="User Avatar"
           className="w-28 h-28 rounded-full mx-auto mb-4 object-cover border-4 border-white shadow-md"
         />
@@ -59,7 +47,7 @@ function Slide({ img, name, text }) {
         <p className="text-gray-600 text-lg mb-2">{text}</p>
         <p className="font-semibold font-lindsey text-gray-800">{name}</p>
         <div className="absolute bottom-[-10px] left-1/2 transform -translate-x-1/2 w-6 h-6 bg-white rotate-45" />
-      </div>
+      </a>
     </div>
   );
 }
@@ -86,7 +74,7 @@ export default function SwiperClient() {
           centeredSlides: true,
         },
         1025: {
-          slidesPerView: 3,
+          slidesPerView: 1,
           centeredSlides: true,
         },
       },
@@ -106,6 +94,7 @@ export default function SwiperClient() {
             img={slide.img}
             name={slide.name}
             text={slide.text}
+            url={slide.url}
           />
         ))}
       </div>

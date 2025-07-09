0cf61a2adcffc2fb9ce80ff4f7cf020112b33f38
CristianMeSu <cristian.mendoza.026@gmail.com>
2025-07-09 17:20:25 -0500

    chore: add @astrojs/netlify dependency to package.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,11 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 import dotenv from 'dotenv';
+import netlify from "@astrojs/netlify";
 dotenv.config();
 export default defineConfig({
   output: "server",
+  adapter: netlify(),
   integrations: [
     tailwind(),
     react(),
